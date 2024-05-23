4b2723cae81822749b87e3fcc9ed1947169d658f
Artur Szulc <artur.szulc222@gmail.com>
2024-05-23 23:57:27 +0200

    Separate layouts. Create routing group for main app pages. Code refactoring.

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+import { FC } from "react";
+
+const Home: FC = () => {
   return (
     <main className="bg-primary-gray dark:bg-dark-primary-gray">
       <h1>Homepage</h1>
     </main>
   );
-}
+};
+export default Home;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata, NextPage } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
-import Header from "@/app/components/header/Header";
-import Footer from "@/app/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +17,7 @@ const RootLayout: NextPage<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
